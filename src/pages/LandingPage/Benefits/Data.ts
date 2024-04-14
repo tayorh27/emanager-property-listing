197790d95564a33e6699073b88e0ec3534ac00f3
johnaribatise1217 <johnaribatise@hsass.org>
2024-04-14 15:31:30 +0100

    finished benefits section landing page completed

---
+++ src/pages/LandingPage/Benefits/Data.ts
@@ -0,0 +1,12 @@
+export const benefitText = [
+  "Save time and effort by managing all property-related tasks in one centralized platform.",
+  "Improve communication and responsiveness with tenants, ensuring their needs are promptly addressed.",
+  "Stay organized with ease access to lease details.",
+  "Gain valuable insights into property performance and make informed decisions for better profitablity."
+]
+
+export const searchText = [
+  "Find your ideal apartment easily with our intuitive apartment search feature, saving you time and effort.",
+  "Experience hassle-free rent payments with secure online transactions.",
+  "Enjoy transparent communication with your property owner/manager, enhancing your overall renting experience."
+]
