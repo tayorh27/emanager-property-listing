0ee068c35033a3bf287c16809604cc8ee0b8d5e1
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-04-26 16:34:49 +0100

    Update PropertyDetails.jsx

--- src/pages/LandingPage/FeaturedProperties/PropertyDetails.jsx
+++ src/pages/LandingPage/FeaturedProperties/PropertyDetails.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { Data } from "../../../Lib/CardData";
 import { FaLocationDot } from "react-icons/fa6";
 import propertyImg  from "../../../assets/img/testAssets/residence.png";
 
 export default function PropertyDetails() {
-  const { productId } = useParams();
-  const filteredProducts = Data.filter((product) => product.id === productId);
+    const { productId } = useParams();
+    const [productInner, setProductInner] = useState("")
+    const filteredProducts = Data.filter((product) => product.id === productId);
+    const handleInnerProduct = (selection) => {
+        setProductInner(selection)
+    }
 
   return (
     <div className="bg-white pt-[100px] px-[97px]">
@@ -15,12 +19,12 @@ export default function PropertyDetails() {
         <FaLocationDot /> Luxury Five-Bedroom Apartment{" "}
       </p>
       <div className="flex bg-[#F6F8F9] text-[16px] font-[500] items-center justify-around">
-        <span className="p-[24px]">Overview</span>
-        <span className="p-[24px]">Features</span>
-        <span className="p-[24px]">Media</span>
-        <span className="p-[24px]">Area Guide</span>
+        <span className="p-[24px]" onClick={handleInnerProduct("overview")}>Overview</span>
+        <span className="p-[24px]" onClick={handleInnerProduct("features")}>Features</span>
+        <span className="p-[24px]" onClick={handleInnerProduct("media")}>Media</span>
+        <span className="p-[24px]" onClick={handleInnerProduct("areaguide")}>Area Guide</span>
           </div>
-          
+
           <div className="w-full">
               <img src={propertyImg} alt="" className="w-full h-full" />
           </div>
