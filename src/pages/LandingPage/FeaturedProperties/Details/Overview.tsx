6fd5f86afe94c478564aa18ece5eb17e7fa004bf
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-04-27 04:24:37 +0100

    Update Overview.tsx

--- src/pages/LandingPage/FeaturedProperties/Details/Overview.tsx
+++ src/pages/LandingPage/FeaturedProperties/Details/Overview.tsx
@@ -6,7 +6,22 @@ export default function Overview() {
   const { productId } = useParams();
     return (
       <div className="flex flex-col w-full">
-          <img src={propertyImg} alt="" className="w-full h-full" />
+        <img src={propertyImg} alt="" className="w-full h-full" />
+        Carousel
+        <div className="flex items-center justify-between w-full">
+          <span className="flex w-full items-center text-center justify-center text-[40px] font-[600]">
+            N1,000,000
+          </span>
+          <span className="flex w-full items-center text-center justify-center">
+            4 beds
+          </span>
+          <span className="flex w-full items-center text-center justify-center">
+            3 baths
+          </span>
+          <span className="flex w-full items-center text-center justify-center">
+            224m<sup>2</sup> per room
+          </span>
         </div>
+      </div>
     );
 }
